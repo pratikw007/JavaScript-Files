7df3d22ff4103eef5856fd7a8058e3128045cab4
Pratik Waghmare <waghmarepratik287@gmail.com>
2022-08-30 21:37:34 +0530

    Js Files

---
+++ Script.js
@@ -0,0 +1,38 @@
+// var nums = [20,30,50,100,200];
+
+// var sum = 0;
+// for(var i = 0; i < nums.length; i++){
+//     sum = sum + nums[i];
+//     console.log("sum =", sum);
+// }
+// console.log(sum);
+
+//Join the Array 
+// var arr =["Newton","School","School","nagpur"];
+// console.log(arr.join("$"));
+    // var arr = ["newton","School","delhi","online"];
+//for...of loops 
+// for (let item of arr){
+//     console.log(item);
+// }
+
+// var nums=[10,20,30,40,50];
+// for (var n of nums) {
+//     console.log(n+100);
+// }
+//next line 
+// console.log("heelo \n world")
+//for... in loops
+var car ={
+    name:'tata',
+    color:'Red',
+    wheel: 'four'
+};
+// console.log()
+for(var x in car){
+    if(x == "wheels"){
+        
+    }
+    // console.log(x,car[x]);
+    console.log(car[x]);
+}
